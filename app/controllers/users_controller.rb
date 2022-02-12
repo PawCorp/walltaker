4ffc50650fc32af3cc57bd18bf24adfc3e6fa154
Pup Gray <pupgray@outlook.com>
2022-02-12 16:31:53 -0500

    Add sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: 'Thank you for signing up!'
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  include ActiveModel::SecurePassword
   has_secure_password
 
-  attr_accessor :email, :password, :password_confirmation
+  validates_uniqueness_of :email, :username
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                              message: 'must be a valid email address' }
+  validates :password, confirmation: true
+  validates :username, presence: true
 end
