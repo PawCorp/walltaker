0e32f305bddc347b1daf371ea12ce90dff0eb6fc
Pup Gray <pupgray@outlook.com>
2022-02-18 00:39:33 -0500

    Added a notification bullet for friend requests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def has_requests?
+    return Friendship.all.where(receiver_id: current_user.id, confirmed: false).count.positive? if current_user
+
+    false
+  end
 end
