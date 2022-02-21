08bbefef7457f0e6baed27d97f1aeebd2b41531a
Pup Gray <pupgray@outlook.com>
2022-02-20 22:50:11 -0500

    WT-T-16 Better validation on usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
                               message: 'must be a valid email address' }
   validates :password, confirmation: true
-  validates :username, presence: true
+  validates :username, presence: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
