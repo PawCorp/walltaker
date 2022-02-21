9ddaae7b5b239db6e021dfd17d25da54292ee6d1
Pup Gray <pupgray@outlook.com>
2022-02-20 22:49:58 -0500

    WT-T-16 User page accessed with username for a list of links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find_by(username: params[:username])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
