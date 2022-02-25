f75685468af5f361902f4939d32c1a4b266918d3
Pup Gray <pupgray@outlook.com>
2022-02-24 21:54:27 -0500

    WT-T-19 Set up a basic icon for the last pinging client

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,6 +2,15 @@ module LinksHelper
   def link_id_for_decoration(link_id)
     return '🐇' if link_id == 8
     return '🐕' if link_id == 1
+
     link_id
+  end
+
+  def link_agent_to_icon(link_agent)
+    return :unknown if link_agent.nil?
+    return :desktop if link_agent.include? 'Walltaker Go Client/'
+    return :android if link_agent.include? 'walltaker-android-client/'
+
+    :unknown
   end
 end
