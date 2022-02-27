63bf9bbc7001ce47b99e3bb2c954fb3724348b49
Pup Gray <pupgray@outlook.com>
2022-02-26 19:11:37 -0500

    total number of links on dashboard lol

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    @online_links_count = Link.all
+                              .where(friends_only: false)
+                              .where('last_ping > ?', Time.now - 1.minute)
+                              .and(
+                                Link.all.where('expires > ?', Time.now).or(Link.all.where(never_expires: true))
+                              ).count
   end
 end
