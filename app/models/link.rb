1c962577326a1cbd0f5d9c2c38b789f30eb46011
Pup Gray <pupgray@outlook.com>
2022-02-27 20:55:17 -0500

    holy shit, streaming live updates to links over websockets, hella easy

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,8 @@ class Link < ApplicationRecord
   belongs_to :user
   has_many :past_links
   validates :expires, presence: true, unless: :never_expires?
+
+  after_update_commit do
+    broadcast_update
+  end
 end
