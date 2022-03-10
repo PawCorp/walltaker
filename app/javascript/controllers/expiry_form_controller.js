5694727843e22b3bba3fdc361d897a77cee829c2
Pup Gray <pupgray@outlook.com>
2022-03-10 00:06:12 -0500

    Disabling the expiry field when the never expires option is checked

---
+++ app/javascript/controllers/expiry_form_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ['switch', 'input']
+
+    connect() {
+        this.enabled = !this.switchTarget.checked
+        this.set()
+    }
+
+    toggle () {
+        this.enabled = !this.enabled
+        this.set()
+    }
+
+    set () {
+        if (!this.enabled) this.inputTarget.value = ''
+        this.switchTarget.checked = !this.enabled
+        this.inputTarget.disabled = !this.enabled
+    }
+}
