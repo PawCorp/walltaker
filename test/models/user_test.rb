7aa58f835bf7f63431a605f433c998385ab512c5
Pup Gray <pupgray@outlook.com>
2022-03-12 01:31:26 -0500

    Another kinda pointless test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'no punctuation in usernames' do
+    user = User.new
+    user.username = '!what ?'
+    assert_not user.valid?
+  end
 end
