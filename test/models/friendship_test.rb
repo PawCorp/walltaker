a2fc8ccc16f8d5c50ba688a93912698a7f83e6ce
Pup Gray <pupgray@outlook.com>
2022-03-12 00:47:58 -0500

    Valid tests

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,19 @@
-require "test_helper"
+require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can't be friends with self" do
+    user_id = 69
+
+    friendship = Friendship.new
+    friendship.receiver_id = user_id
+    friendship.sender_id = user_id
+    assert_not friendship.save, 'Saved a friendship where the receiver and sender are the same'
+  end
+  test "can't be friends with no one" do
+    user_id = 69
+
+    friendship = Friendship.new
+    friendship.sender_id = user_id
+    assert_not friendship.save, 'Saved a friendship where the sender is nil'
+  end
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'expiry date required when not never_expires' do
+    link = Link.new
+    link.never_expires = false
+    link.expires = nil
+
+    assert_not link.save, 'saved an expiring link with a nil expiry date'
+  end
+  test 'theme should not have spaces' do
+    link = Link.new
+    link.never_expires = true
+    link.theme = 'this is a multi tag theme'
+
+    assert_not link.save, 'saved a theme with multiple tags'
+  end
 end
