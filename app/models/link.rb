b1d9d1b49c7cdbf38467d4b9dee7fca651d6255e
Pup Gray <pupgray@outlook.com>
2022-03-13 20:26:30 -0400

    Only emitting change event for links on action cable when something we show changes. This means that the online status is no longer live

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,7 +6,9 @@ class Link < ApplicationRecord
   validates :theme, format: { without: /\s+/i, message: 'must be only 1 tag.' }
 
   after_update_commit do
-    broadcast_update
+    if blacklist_previously_changed? || terms_previously_changed? || theme_previously_changed? || response_text_previously_changed? || last_ping_user_agent_previously_changed? || expires_previously_changed? || never_expires_previously_changed? || friends_only_previously_changed? || post_url_previously_changed?
+      broadcast_update
+    end
     if previous_changes['post_url']&.include?(post_url)
       clear_changes_information
       broadcast_replace_to "submit_link_#{id}", target: "submit_link_#{id}", partial: 'links/submit', locals: { link: self }
