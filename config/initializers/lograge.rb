23931e4130aa75f8fc6204d27de2d8db26264c10
Pup Gray <pupgray@outlook.com>
2022-04-06 23:50:39 -0400

    Another small change to lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,4 +1,10 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
+  config.lograge.custom_options = lambda do |event|
+    {
+      exception: event.payload[:exception], # ["ExceptionClass", "the message"]
+      exception_object: event.payload[:exception_object] # the exception instance
+    }
+  end
 end
