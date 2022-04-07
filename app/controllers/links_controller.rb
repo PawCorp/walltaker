f13cc813ff657823392993681c03b7cffc42ccca
Pup Gray <pupgray@outlook.com>
2022-04-07 13:21:07 -0400

    Save extra wallpaper-engine client user agent header

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -220,6 +220,7 @@ class LinksController < ApplicationController
       @link.last_ping_user_agent = request.user_agent if request.user_agent
       @link.last_ping_user_agent = @link.last_ping_user_agent + ' ' + request.headers['joihow'] if @link.last_ping_user_agent && request.headers['joihow']
       @link.last_ping_user_agent = @link.last_ping_user_agent + ' ' + request.headers['User_Agent'] if @link.last_ping_user_agent && request.headers['User_Agent']
+      @link.last_ping_user_agent = @link.last_ping_user_agent + ' ' + request.headers['Wallpaper-Engine-Client'] if @link.last_ping_user_agent && request.headers['Wallpaper-Engine-Client']
       @link.save
     end
   end
