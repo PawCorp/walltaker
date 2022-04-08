aa471d7bf1ab9fa91455c3d518bd1d57f11daf95
Pup Gray <pupgray@outlook.com>
2022-04-07 20:22:28 -0400

    Confirm unfriending with modal before calling api

---
+++ app/javascript/controllers/friendship_controller.js
@@ -0,0 +1,17 @@
+import {Controller} from "@hotwired/stimulus"
+import {WithModal} from '../modules/Modal'
+
+class FriendshipController extends Controller {
+    static targets = ['cancel']
+
+    confirm() {
+        this.modal.open()
+        this.cancelTarget.focus()
+    }
+
+    cancel() {
+        this.modal.close()
+    }
+}
+
+export default WithModal(FriendshipController);
