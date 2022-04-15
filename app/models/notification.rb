08b84990b5bd506c346f3ed9cd666e8767ce5e79
Pup Gray <pupgray@outlook.com>
2022-04-15 11:03:52 -0400

    added live notification feeds, holy shit turbo streams are cool

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,8 @@
 class Notification < ApplicationRecord
   belongs_to :user
   enum notification_type: %i[friend_request_sent friend_request_received friend_request_they_accepted post_response]
+
+  after_commit do
+    broadcast_replace_to "header_notifications_#{user.id}", target: "header_notifications", partial: 'notifications', locals: { notifications: Notification.all.where(user: user).order(id: :desc).limit(5) }
+  end
 end
