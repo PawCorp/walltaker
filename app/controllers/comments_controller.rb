4b404d9ffb0d36a3ab47c5f36e465d16387ff7a0
Pup Gray <pupgray@outlook.com>
2022-04-30 11:23:28 -0400

    Cleaning up unused actions in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
   before_action :set_link, only: %i[ new create ]
 
   # GET /links/1/comments
@@ -7,17 +6,9 @@ class CommentsController < ApplicationController
     @comments = Comment.all.where(link_id: params['link_id'])
   end
 
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
   # GET /links/1/comments/new
   def new
     @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
   end
 
   # POST /comments or /comments.json
@@ -33,22 +24,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to link_comments_url, notice: "Comment was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     def set_link
       @link = Link.find(params[:link_id])
     end
--- app/views/comments/_comment.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! comment, :id, :link_id, :user_id, :content, :created_at, :updated_at
-json.url comment_url(comment, format: :json)
