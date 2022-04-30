ab22be3346eec30dff71de9b58b9c34a68258ab6
Pup Gray <pupgray@outlook.com>
2022-04-30 11:18:50 -0400

    Fully working chat

---
+++ app/javascript/controllers/new_comment_form_controller.js
@@ -0,0 +1,30 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class NewCommentFormController extends Controller {
+    static targets = ['send', 'input']
+
+    connect() {
+        this.element.addEventListener('submit', this.focusInput.bind(this))
+    }
+
+    focusInput() {
+        if (this.sendTarget && this.inputTarget) {
+            this.sendTarget.blur()
+            setTimeout(() => this.inputTarget.focus(), 0)
+        }
+    }
+
+    inputChange(e) {
+        if (e.target) {
+            const valid = e.target.value.length > 0
+            console.log(valid, e.target.value)
+            this.setDisabled(!valid)
+        }
+    }
+
+    setDisabled(isDisabled) {
+        if (this.sendTarget) {
+            this.sendTarget.disabled = isDisabled
+        }
+    }
+}
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :link
   belongs_to :user
+  validates :content, presence: true
+
+  after_commit do
+    broadcast_prepend_to "link_comments_#{link.id}", target: "link_comments", partial: 'comments/comment', locals: { comment: self }
+  end
 end
