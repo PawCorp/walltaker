07ed87727b88bb3403e22170db642617480a74da
Pup Gray <pupgray@outlook.com>
2022-05-07 13:22:46 -0400

    Fixes new api presence logging

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,11 @@ Rails.application.routes.draw do
   post 'users/:username/key', to: 'users#new_api_key', as: 'user_new_api_key'
   get 'notifications/:id', to: 'notification#show', as: 'notification'
 
-  get 'api/links/:id.json', to: 'api#show_link'
-  post 'api/links/:id/response.json', to: 'api#set_link_response'
-  get 'api/users/:username.json', to: 'api#show_user', as: 'user_status'
+  defaults format: :json do
+    get 'api/links/:id.json', to: 'api#show_link'
+    post 'api/links/:id/response.json', to: 'api#set_link_response'
+    get 'api/users/:username.json', to: 'api#show_user', as: 'user_status'
+  end
 
   resources :users
   resources :session
