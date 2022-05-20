4e2419c11a16ccb3adabe6e64da544efccf1dc25
Pup Gray <pupgray@outlook.com>
2022-05-19 23:24:40 -0400

    Added a slightly janky confirmation when link is recently set for porn search, closes #32

---
+++ app/javascript/controllers/porn_search_controller.js
@@ -0,0 +1,42 @@
+import {Controller} from "@hotwired/stimulus"
+import {WithModal} from '../modules/Modal'
+
+class PornSearchController extends Controller {
+    static targets = ['thumbnail']
+    static values = {
+        secondsSinceLastWallpaper: Number
+    }
+
+    connect () {
+        this.timer = setInterval(() => {
+            this.secondsSinceLastWallpaperValue++
+        }, 1000)
+    }
+
+    disconnect () {
+        clearInterval(this.timer)
+    }
+
+    click_thumbnail (e) {
+        if (this.secondsSinceLastWallpaperValue && this.secondsSinceLastWallpaperValue < 60) {
+            e.preventDefault()
+            e.stopPropagation()
+            this.modal.open()
+            this.thumbnailToConfirm = e.target
+        }
+    }
+
+    cancel () {
+        this.modal.close()
+    }
+
+    confirm () {
+        if (this.thumbnailToConfirm) {
+            const form = this.thumbnailToConfirm.closest('form')
+            if (form) form.requestSubmit()
+        }
+        this.modal.close()
+    }
+}
+
+export default WithModal(PornSearchController);
