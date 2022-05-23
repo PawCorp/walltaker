20ad51fc47699862dba44ed27e31d27e06e33236
Pup Gray <pupgray@outlook.com>
2022-05-23 18:35:46 -0400

    🐈

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,6 +2,7 @@ module LinksHelper
   def link_id_for_decoration(link_id)
     return '🐇' if link_id == 69
     return '🐕' if link_id == 1
+    return '🐈' if link_id == 658 || link_id == 656
 
     link_id
   end
