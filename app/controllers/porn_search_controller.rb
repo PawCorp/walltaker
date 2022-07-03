83bebd8dc435dda08e3b5b51e2c89de50a40f03d
Pup Gray <pupgray@outlook.com>
2022-07-03 17:37:40 -0400

    Added a back button to the porn search

--- app/controllers/porn_search_controller.rb
+++ app/controllers/porn_search_controller.rb
@@ -9,7 +9,7 @@ class PornSearchController < ApplicationController
     append_to_tags = ''
     append_to_tags += @link.theme if (@link.theme)
     append_to_tags += ' ' + ((sanitized_blacklist.split.map { |tag| "-#{tag}"}).join ' ') unless (sanitized_blacklist.empty?)
-    @posts = get_tag_results porn_search_params[:tags], porn_search_params[:after], append_to_tags
+    @posts = get_tag_results porn_search_params[:tags], porn_search_params[:after], porn_search_params[:before], append_to_tags
     @last_tags = porn_search_params[:tags]
 
     track :regular, :search_e621_on_link, search: porn_search_params[:tags]
@@ -19,7 +19,7 @@ class PornSearchController < ApplicationController
 
   private
 
-  def get_tag_results(tag_string, after, append_to_tags)
+  def get_tag_results(tag_string, after, before, append_to_tags)
     padded_tag_string = tag_string + ' type:jpg type:png'
     unless append_to_tags.nil? || append_to_tags.empty?
       padded_tag_string = "#{padded_tag_string} #{append_to_tags.to_s}"
@@ -28,6 +28,8 @@ class PornSearchController < ApplicationController
     url = "https://e621.net/posts.json?tags=#{tags}&limit=15"
     after_id = after.gsub(/\D/, '') if after
     url = "#{url}&page=b#{after_id}" if after_id
+    before_id = before.gsub(/\D/, '') if before
+    url = "#{url}&page=a#{before_id}" if before_id
     response = Excon.get(url, headers: { 'User-Agent': 'walltaker.joi.how (by ailurus on e621)' })
     if response.status != 200
       track :error, :e621_posts_api_fail, response: response
@@ -39,6 +41,6 @@ class PornSearchController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def porn_search_params
-    params.permit(:tags, :after, :link)
+    params.permit(:tags, :after, :before, :link)
   end
 end
