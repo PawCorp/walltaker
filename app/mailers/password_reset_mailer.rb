c72395d2aea00f893844f04d682246193ee6a35e
Pup Gray <pupgray@outlook.com>
2022-09-17 16:17:43 -0400

    using uuids instead

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -4,7 +4,7 @@ class PasswordResetMailer < ApplicationMailer
   # @param [User] user
   def reset_password(user)
     @user = user
-    @user.password_reset_token = SecureRandom.base64(30).slice 0..20
+    @user.password_reset_token = SecureRandom.uuid
     @user.save
     mail( :to => @user.email,
           :subject => 'Walltaker Password Reset' )
