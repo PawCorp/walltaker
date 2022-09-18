e2345736abaac280b3798b1a2608bdff0222c407
Pup Gray <pupgray@outlook.com>
2022-09-18 13:18:47 -0400

    Using anchor tag again on link lists

--- app/javascript/controllers/go_controller.js
+++ app/javascript/controllers/go_controller.js
@@ -1,23 +1,16 @@
 import {Controller} from "@hotwired/stimulus"
 
+const controls = document.createElement('a')
+controls.className = 'link--anchor-shade'
+
 export default class GoController extends Controller {
     static values = {
         to: String
     }
 
     connect () {
-        if (this.element) {
-            if (this.element.classList) { this.element.classList.add('clickable') }
-            this.element.setAttribute('role', 'link')
-            this.element.setAttribute('tabindex', '0')
-            this.element.addEventListener('click', this.go.bind(this))
-            this.element.addEventListener('keypress', this.go.bind(this))
-        }
-    }
-
-    go(e) {
-        if (this.toValue && ((e.key === 'Enter') || (e.key === ' ') || (e.key === undefined))) {
-            window.location = this.toValue;
-        }
+        this.element.className = 'link--anchor-shade-container'
+        controls.href = this.toValue
+        this.element.prepend(controls)
     }
 }
