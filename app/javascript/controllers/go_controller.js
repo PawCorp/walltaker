cad4d28023726b91a7e20d172a074d9034c01665
Pup Gray <pupgray@outlook.com>
2022-09-18 13:25:40 -0400

    debug

--- app/javascript/controllers/go_controller.js
+++ app/javascript/controllers/go_controller.js
@@ -10,7 +10,10 @@ export default class GoController extends Controller {
 
     connect () {
         this.element.className = 'link--anchor-shade-container'
+        console.log('here', this.element)
         controls.href = this.toValue
+        console.log(controls, this.element)
         this.element.prepend(controls)
+        console.log('prepended', this.element)
     }
 }
