104bf267f9b05f29d6e4682da277590edcf9d7c1
Pup Gray <pupgray@outlook.com>
2022-09-23 21:15:08 -0400

    AHA! Finally fixed right click to open

--- app/javascript/controllers/go_controller.js
+++ app/javascript/controllers/go_controller.js
@@ -1,7 +1,11 @@
 import {Controller} from "@hotwired/stimulus"
 
-const controls = document.createElement('a')
-controls.className = 'link--anchor-shade'
+function getControls () {
+    const controls = document.createElement('a')
+    controls.className = 'link--anchor-shade'
+
+    return controls
+}
 
 export default class GoController extends Controller {
     static values = {
@@ -9,11 +13,12 @@ export default class GoController extends Controller {
     }
 
     connect () {
-        this.element.className = 'link--anchor-shade-container'
-        controls.href = this.toValue
-        this.element.prepend(controls)
-
         if (this.element) {
+            this.element.className = 'link--anchor-shade-container'
+            const controls = getControls()
+            controls.href = this.toValue
+            this.element.prepend(controls)
+
             if (this.element.classList) { this.element.classList.add('clickable') }
             this.element.setAttribute('role', 'link')
             this.element.setAttribute('tabindex', '0')
