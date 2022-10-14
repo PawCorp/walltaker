dbf7db5b543d66efac72278915479f0c81f86b58
Pup Gray <pupgray@outlook.com>
2022-10-13 22:00:36 -0400

    Added a copy link button

--- app/javascript/controllers/link_controller.js
+++ app/javascript/controllers/link_controller.js
@@ -1,8 +1,70 @@
 import {Controller} from "@hotwired/stimulus"
 import {WithModal} from '../modules/Modal'
 
+function copy(str) {
+    if (navigator?.clipboard?.writeText) {
+        navigator.clipboard.writeText(str)
+    } else {
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
+};
+
 class LinkController extends Controller {
-    static targets = ['cancel']
+    static targets = ['cancel', 'copy', 'shareIcon', 'checkmarkIcon']
+    static values = {
+        id: Number
+    }
+
+    connect() {
+        if (this.copyTarget) {
+            this.copyTarget.addEventListener('click', this.copyLink.bind(this))
+        }
+
+        if (this.checkmarkIconTarget) {
+            this.checkmarkIconTarget.style.display = 'none'
+        }
+    }
+
+    disconnect() {
+        if (this.copyTarget) {
+            this.copyTarget.removeEventListener('click', this.copyLink.bind(this))
+        }
+    }
+
+    copyLink(e) {
+        e.preventDefault()
+        try {
+            copy(`https://walltaker.joi.how/links/${this.idValue}`)
+            if (this.checkmarkIconTarget) {
+                this.checkmarkIconTarget.style.display = 'inline-block'
+            }
+            if (this.shareIconTarget) {
+                this.shareIconTarget.style.display = 'none'
+            }
+            if (this.copyTarget) {
+                this.copyTarget.innerText = 'Copied!'
+            }
+        } catch (e) {
+            console.warn(e)
+            if (this.checkmarkIconTarget) {
+                this.checkmarkIconTarget.style.display = 'none'
+            }
+            if (this.shareIconTarget) {
+                this.shareIconTarget.style.display = 'inline-block'
+            }
+            if (this.copyTarget) {
+                this.copyTarget.innerText = 'Not supported :('
+            }
+        }
+    }
 
     confirm() {
         this.modal.open()
