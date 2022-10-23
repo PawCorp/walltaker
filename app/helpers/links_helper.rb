461ceebfaedb4a90f4ec5afacfabef30cee3fcf9
Pup Gray <pupgray@outlook.com>
2022-10-23 14:29:43 -0400

    Added gio's client

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -16,6 +16,7 @@ module LinksHelper
     return :wallpaper_engine if link_agent.include? 'Wallpaper-Engine-Client'
     return :automate if link_agent.include? 'walltaker-android-automate'
     return :swift if link_agent.include? 'CFNetwork/'
+    return :android_changer if link_agent.include? 'Walltaker-Changer/'
 
     :unknown
   end
