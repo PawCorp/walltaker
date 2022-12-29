4a220df4e628b3513be44a86deca139e6c8362a1
Pup Gray <pupgray@outlook.com>
2022-12-29 10:39:40 -0500

    Stub email for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,8 +55,8 @@ class UsersController < ApplicationController
         throw :nefarious
       end
 
-      PasswordResetMailer.reset_password(user).deliver
-      redirect_to login_path, notice: 'A password reset email has been sent to that user'
+      #PasswordResetMailer.reset_password(user).deliver
+      redirect_to login_path, notice: 'Contact Gray! The email delivery service is down.'
     rescue
       track :nefarious, :failed_to_reset_password, tried_email: params['email']
       redirect_to forgor_path, alert: 'User was not found with that email'
