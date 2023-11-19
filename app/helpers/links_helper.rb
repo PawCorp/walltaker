f06df7ff16f2ddaf4040354fea2ff78bd8b502ef
Pup Gray <pupgray@outlook.com>
2023-11-19 17:32:59 -0500

    New arson automate client

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -19,6 +19,7 @@ module LinksHelper
     return :joihow if link_agent.include? 'joihow'
     return :wallpaper_engine if link_agent.include? 'Wallpaper-Engine-Client'
     return :automate if link_agent.include? 'walltaker-android-automate'
+    return :arson_automate if link_agent.include? 'arson-walltaker-automate'
     return :ioswidget if link_agent.include? 'widgetExtension'
     return :swift if link_agent.include? 'CFNetwork/'
     return :android_changer if link_agent.include? 'Walltaker-Changer/'
