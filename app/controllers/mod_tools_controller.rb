92a207f3840952bd3b09fa9da62c20a354f59209
Pup Gray <pupgray@outlook.com>
2023-12-17 20:00:36 -0500

    More powerful password reset mod tool, you can search tiny strings

--- app/controllers/mod_tools_controller.rb
+++ app/controllers/mod_tools_controller.rb
@@ -10,12 +10,12 @@ class ModToolsController < ApplicationController
   def update_password_reset
     email = params.permit(:email)['email']
 
-    if !email || email.length < 6
+    if !email || email.length < 2
       return redirect_to mod_tools_passwords_index_path(fail: 'Email segment too short. Must be at least 6 letters. They need to provide more of their email.', email:)
     end
 
     # Prepared statement, rails escapes and wraps template var here.
-    matches = User.where('email LIKE ?', "%#{email}%").all
+    matches = User.where('lower(email) LIKE ?', "%#{email.downcase}%").all
 
     if matches.length > 1
       return redirect_to mod_tools_passwords_index_path(fail: 'Matches more than 1 account, can they be more specific?', email:)
@@ -27,7 +27,12 @@ class ModToolsController < ApplicationController
 
     matches[0].password_reset_token = SecureRandom.uuid
     matches[0].save
-    redirect_to mod_tools_passwords_index_path(link: matches[0].password_reset_token, username: matches[0].username, email:)
+
+    if email.length < 6
+      return redirect_to mod_tools_passwords_index_path(link: matches[0].password_reset_token, username: matches[0].username, fail: "Search provided was short. (Only #{email.length} letters!) Are you sure you aren\'t being manipulated? Someone may be able to guess a small portion of an email.", email: matches[0].email)
+    end
+
+    redirect_to mod_tools_passwords_index_path(link: matches[0].password_reset_token, username: matches[0].username, email: matches[0].email)
   end
 
   def show_user
