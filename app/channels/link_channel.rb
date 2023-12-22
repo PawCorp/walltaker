28296c5b7577eb8aa36d9f46cf2443bd6eb2a107
Pup Gray <pupgray@outlook.com>
2023-12-22 10:23:28 -0500

    Add a check to the link channel

--- app/channels/link_channel.rb
+++ app/channels/link_channel.rb
@@ -21,4 +21,21 @@ class LinkChannel < ApplicationCable::Channel
       connection.watched_link.save
     end
   end
+
+  def check
+    if params[:id].present?
+      link = Link.find(params[:id])
+      if link
+        if connection&.watched_link
+          connection.watched_link.live_client_started_at = nil
+          connection.watched_link.save
+        end
+        connection.watched_link = link
+        link.live_client_started_at = Time.now
+        link.save
+
+        link
+      end
+    end
+  end
 end
