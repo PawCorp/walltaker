4837979ee3db28908ab8ff69fbd0aaf2b9d6a2a4
Pup Gray <pupgray@outlook.com>
2023-12-27 00:55:50 -0500

    Hack for now,

--- app/channels/link_channel.rb
+++ app/channels/link_channel.rb
@@ -32,6 +32,7 @@ class LinkChannel < ApplicationCable::Channel
         end
         connection.watched_link = link
         link.live_client_started_at = Time.now
+        link.last_ping_user_agent = Time.now
         link.save
       end
     end
