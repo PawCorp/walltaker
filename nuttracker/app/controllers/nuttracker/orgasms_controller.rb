bf05aa15164cbc2f1ee898056319668b80db74a3
Pup Gray <pupgray@outlook.com>
2024-01-21 16:39:56 -0500

    Recent setters as well

--- nuttracker/app/controllers/nuttracker/orgasms_controller.rb
+++ nuttracker/app/controllers/nuttracker/orgasms_controller.rb
@@ -23,7 +23,9 @@ module Nuttracker
     # GET /orgasms/new
     def new
       @orgasm = Orgasm.new
-      @friends = Friendship.involving(current_user).is_confirmed
+      friends = Friendship.involving(current_user).is_confirmed.map { |f| f.other_user(current_user) }
+      recent_setters = PastLink.joins(:set_by).where(user: current_user).where.not(set_by: nil).select(:set_by_id).group(:set_by_id).limit(10).map { |pl| pl.set_by }
+      @possible_causers = (friends + recent_setters).uniq
     end
 
     # GET /orgasms/1/edit
