82498059cf99853767b0b840b00db636c4271495
Xeno <81916167+XenoPanther@users.noreply.github.com>
2024-02-15 18:26:12 +0000

    Didn't sync changes beforehand, readded kem's walltaker

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -25,8 +25,8 @@ module LinksHelper
     return :android_changer if link_agent.include? 'Walltaker-Changer/'
     return :jberliner if link_agent.include? 'JBerliner'
     return :chewtoy if link_agent.include? 'WalltakerEngine-chewtoy/'
+    return :kemkem_userscript if link_agent.include? 'Walltaker for Walltaker (kemkem)'
     return :xenofluff if link_agent.include? 'Walltaker_eXPerience'
-
     :unknown
   end
 
