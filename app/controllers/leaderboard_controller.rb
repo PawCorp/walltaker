982d183f68dad66cb79832b457c5483bf269eab2
Pup Gray <pupgray@outlook.com>
2024-02-17 12:48:44 -0500

    Just removing DeletedAccount28 from the leaderboard atm

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,5 @@
 class LeaderboardController < ApplicationController
   def index
-    @top_setters = User.where.not(username: %w[PornLizardKi PornLizardWarren PornLizardTaylor PornBot]).order(set_count: :desc).limit(10)
+    @top_setters = User.where.not(username: %w[PornLizardKi PornLizardWarren PornLizardTaylor PornBot DeletedAccount28]).order(set_count: :desc).limit(10)
   end
 end
