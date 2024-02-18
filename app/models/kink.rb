e18cc0d1e64851bcd64fe791d953986a782e445c
Pup Gray <pupgray@outlook.com>
2024-02-18 13:32:51 -0500

    Missed this!

--- app/models/kink.rb
+++ app/models/kink.rb
@@ -2,7 +2,7 @@ class Kink < ApplicationRecord
   validates_uniqueness_of :name
   validates_length_of :name, maximum: 30, minimum: 1
 
-  normalizes :name, with: -> name { name.gsub(/[^\w\d_\-\(\)\/\s]/, '').strip.squish.gsub(/\s/, '_') }
+  normalizes :name, with: -> name { name.gsub(/[^\w\d_\-\(\)\/\s]/, '').strip.squish.downcase.gsub(/\s/, '_') }
 
   has_many :kink_havers
   has_many :users, through: :kink_havers
