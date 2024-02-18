610d4e55e626e09fb4a607503eb2e6468cfc24db
Pup Gray <pupgray@outlook.com>
2024-02-18 12:16:15 -0500

    Better normalization for kink names

--- app/models/kink.rb
+++ app/models/kink.rb
@@ -2,7 +2,7 @@ class Kink < ApplicationRecord
   validates_uniqueness_of :name
   validates_length_of :name, maximum: 30, minimum: 1
 
-  normalizes :name, with: -> name { name.titleize.gsub(/[^\w\d_]/, '').underscore }
+  normalizes :name, with: -> name { name.gsub(/[^\w\d_\-\(\)\/\s]/, '').strip.squish.gsub(/\s/, '_') }
 
   has_many :kink_havers
   has_many :users, through: :kink_havers
